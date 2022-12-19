6ffcf7794e88f1104e45dd00f73e0267c22b4d30
CJ Mcclure <callum.mcclure98@gmail.com>
2022-12-19 22:26:52 +0000

    add padding and image spacing

--- components/sections/projectSummaries.tsx
+++ components/sections/projectSummaries.tsx
@@ -32,15 +32,28 @@ export default function ProjectSummaries(props: ProjectSummariesProps) {
   };
 
   return (
-    // @ts-ignore - type complex
-    <Box>
+    // @ts-ignore - type complexadd
+    <Box sx={{ pb: 2 }}>
       <Divider sx={{ bgcolor: "secondary.dark" }} />
       <Typography variant="h2" component="h2" gutterBottom id="projects">
         Projects
       </Typography>
-      <Grid container spacing={2} justifyContent="center" sx={{ pb: 2 }}>
+      <Grid
+        container
+        direction="row"
+        spacing={2}
+        justifyContent="center"
+        sx={{ pb: 2 }}
+      >
         {projects.map((project) => (
-          <Grid item xs={12} sm={6} md={4} key={project.id}>
+          <Grid
+            item
+            xs={12}
+            sm={6}
+            md={4}
+            key={project.id}
+            style={{ display: "flex" }}
+          >
             <Card>
               <CardContent>
                 <Typography variant="h5" component="h2">
@@ -49,9 +62,10 @@ export default function ProjectSummaries(props: ProjectSummariesProps) {
                 {project.image && (
                   <CardMedia
                     component="img"
-                    height="194"
+                    height="200"
                     image={project.image}
                     alt={project.title}
+                    sx={{ pb: 2 }}
                   />
                 )}
                 <Typography
